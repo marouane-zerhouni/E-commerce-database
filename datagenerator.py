f18dc0b7d05880f9a14f212ca91fa7103304b38c
emzed7269 <78227232+emzed7269@users.noreply.github.com>
2023-05-06 10:56:13 +0200

    finished product table

--- datagenerator.py
+++ datagenerator.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import faker_commerce
+from datetime import datetime
 
 f = Faker()
 f.add_provider(faker_commerce.Provider)
@@ -11,6 +12,11 @@ product = [
      'SKU' : f.unique.bothify(text='??-???-###', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
      'category_id' : f.unique.random_int(min=100, max=999),
      'inventory_id' : f.unique.random_int(min=1, max=2500),
+     'price' : int("".join(filter(str.isdigit, f.pricetag()))),
+     'discount_id' : 0,
+     'created_at' : f.date_time_between_dates(datetime_start=datetime.datetime(2022, 1, 1, 00, 00, 00), datetime_end = datetime.now()),
+     'modified_at' : f.date_time_between_dates(datetime_start=product["created_at"], datetime_end = datetime.now()),
+     'deleted_at' : f.date_time_between_dates(datetime_start=product["created_at"], datetime_end = datetime.now())
      } 
      for x in range(500)]
 
